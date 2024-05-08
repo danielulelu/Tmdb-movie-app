8d92a815bbbb84936e7f98edd154ccb65699126b
Eleven <danielulelu@gmail.com>
2024-05-09 00:43:45 +0300

    done about message, which I will change later.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,21 @@
+export default function About() {
+  return (
+    <div className="mx-auto max-w-6xl p-3 space-y-4">
+      <h1 className="text-6xl md:text-8xl font-bold flex items-center justify-center pt-4">
+        Hi there
+      </h1>
+
+      <h2 className="flex items-center justify-center text-3xl pt-10">
+        Let's talk about TMDB
+      </h2>
+      <p className="text-xl md:text-2xl font-light flex items-center justify-center pt-4">
+        TMDB is a website that provides information about movies and tv shows.
+        It has strong breadth of data which is largely unmatched and very
+        precise to the movie community and fraternity. Happy browsing{" "}
+      </p>
+      <button className="rounded-lg px-4 py-2 bg-white text-red-700 uppercase hover:text-white hover:bg-red-800 font-bold text-2xl">
+        contact tmdb
+      </button>
+    </div>
+  );
+}
